ffc45fac944973af2108b297795e2b5b819503e7
NikolaiS1900 <sandbeck1990@protonmail.com>
2024-08-09 22:19:45 +0200

    fre  9 aug 22:19:32 CEST 2024

---
+++ main.py
@@ -0,0 +1,5 @@
+import numpy as np
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.model_selection import train_test_split
