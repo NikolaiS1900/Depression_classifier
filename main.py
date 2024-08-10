a7a7afdf6b17794a24bdd07798fec3d0f464fba9
NikolaiS1900 <sandbeck1990@protonmail.com>
2024-08-10 10:40:39 +0200

    lør 10 aug 10:40:24 CEST 2024

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.model_selection import train_test_split
+
+
+class classifier():
+    def __init__(self):
+        self.df = pd.read_csv("data/Depression/depressive_tweets_processed_cleaned.csv")
+        self.inputs = self.df["text"]
+        self.labels = self.df["labels"]
+
+    def see_df_head(self):
+        print(self.df.head())
+
+    def see_data_size(self):
+        print(len(self.df))
+
+    def histogram(self):
+        self.labels = self.df["labels"]
+
+        # histogram to see the distribution of the labels
+        # and by that we can see if the data is balanced
+        self.labels.hist(figsize=(10, 5))
+
+        plt.show()  # install PyQt5
+
+classifier = classifier()
+classifier = classifier.histogram()
