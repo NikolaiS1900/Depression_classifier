5d6465e8580c2646440807267be9a113c68ab338
NikolaiS1900 <sandbeck1990@protonmail.com>
2024-12-15 15:19:29 +0100

    start update of main.py

--- main.py
+++ main.py
@@ -1,31 +1,42 @@
-from libraries.data_handler import *
-from libraries.vectorizer import *
-from libraries.model_methods import *
+from libraries.data_util_methods import DataUtils
 
-# data_handler = DataHandler()
-# data_handler.concatenate_data("data")
-# data_handler.histogram()
+data_utils = DataUtils()
+data_utils.prepare_data("data/", "concatenated_data/concatenated_data.csv")
+data_utils.get_csv_file_head()
+data_utils.histogram_of_csv_file()
 
-vec = Vectorizer()
-vec.vectorizer("concatenated_data/concatenated_data.csv")
-# vec.get_info_on_sparse_matrix("percentage_non_zeros")
 
-model_methods = ModelMethods()
 
-model_methods.create_model("BernoulliNB")
-model_methods.create_model("ComplementNB")
-model_methods.create_model("MultinomialNB")
 
-model_methods.show_model_info(
-                        "ComplementNB",
-                        scores = False,
-                        show_confusion_matrix = False,
-                        misclassified_classes = False,
-                        importantest_feauture = False,
-                        word_feature_index_map = False,
-                        feature_index_word = False,
-                        idx2word = False,
-                        top10word = False
-                        )
 
-model_methods.predict_new_string("ComplementNB")
+# from libraries.data_handler import *
+# from libraries.vectorizer import *
+# from libraries.model_methods import *
+
+# # data_handler = DataHandler()
+# # data_handler.concatenate_data("data")
+# # data_handler.histogram()
+
+# vec = Vectorizer()
+# vec.vectorizer("concatenated_data/concatenated_data.csv")
+# # vec.get_info_on_sparse_matrix("percentage_non_zeros")
+
+# model_methods = ModelMethods()
+
+# model_methods.create_model("BernoulliNB")
+# model_methods.create_model("ComplementNB")
+# model_methods.create_model("MultinomialNB")
+
+# model_methods.show_model_info(
+#                         "ComplementNB",
+#                         scores = False,
+#                         show_confusion_matrix = False,
+#                         misclassified_classes = False,
+#                         importantest_feauture = False,
+#                         word_feature_index_map = False,
+#                         feature_index_word = False,
+#                         idx2word = False,
+#                         top10word = False
+#                         )
+
+# model_methods.predict_new_string("ComplementNB")
